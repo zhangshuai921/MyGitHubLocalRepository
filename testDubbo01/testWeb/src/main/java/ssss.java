b6d6411e06453d1430b12e466b2edf70aeb6bc21
zhangshuai921 <13121231257@163.com>
2019-01-17 13:10:51 +0800

    ces

---
+++ testDubbo01/testWeb/src/main/java/ssss.java
@@ -0,0 +1,13 @@
+/**
+ * @program: testDubbo01
+ * @description:
+ * @author: zhangshuai
+ * @create: 2019-01-17 13:09
+ **/
+public class ssss {
+
+    public static void main(String[] args) {
+        System.out.println("劳斯莱斯累死了");
+    }
+
+}
